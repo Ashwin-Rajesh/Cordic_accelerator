94c0e16a49bd2d2d2047e1000127b1e0d73979c1
Ashwin <aswinrajesh1@gmail.com>
2022-03-09 20:59:11 +0530

    Removed dnext output from CORDIC core

--- rtl/design.v
+++ rtl/design.v
@@ -6,7 +6,7 @@ module cordic #(
   input signed [p_WIDTH-1:0] i_xprev,
   input signed [p_WIDTH-1:0] i_yprev,
   input signed [p_WIDTH-1:0] i_zprev,
-  input i_dprev,				// 0 for d = -1  1 for d = 1
+  input i_dprev,				// 0 for d = -1 for rotating by -i_lut and 1 for d = 1 for rotating by i_lut
 
   // Control inputs
   input i_mode,					// 0 for hyperbolic, 1 for circular
@@ -20,43 +20,35 @@ module cordic #(
   // Data outputs
   output reg signed [p_WIDTH-1:0] o_xnext,
   output reg signed [p_WIDTH-1:0] o_ynext,
-  output reg signed [p_WIDTH-1:0] o_znext,
-  output o_dnext
+  output reg signed [p_WIDTH-1:0] o_znext
 );
 
   wire signed [p_WIDTH-1:0] w_xshifted = i_xprev >>> i_shift_amnt;
   wire signed [p_WIDTH-1:0] w_yshifted = i_yprev >>> i_shift_amnt;
   
-  // Testing circular mode only
   always @(*) begin
     if(i_mode) begin
       // Circular mode
       if(i_dprev) begin
         o_xnext = i_xprev - w_yshifted;
         o_ynext = i_yprev + w_xshifted;
-
         o_znext = i_zprev - i_lut;
       end else begin
         o_xnext = i_xprev + w_yshifted;
         o_ynext = i_yprev - w_xshifted;
-
         o_znext = i_zprev + i_lut;
       end
     end else begin
+      // Hyperbolic mode
       if(i_dprev) begin
         o_xnext = i_xprev + w_yshifted;
         o_ynext = i_yprev + w_xshifted;
-
         o_znext = i_zprev - i_lut;
       end else begin
         o_xnext = i_xprev - w_yshifted;
         o_ynext = i_yprev - w_xshifted;
-
         o_znext = i_zprev + i_lut;
       end
     end
   end
-
-  assign o_dnext = ~o_znext[31];
-
 endmodule
