37f175777d2bc4074b7a67020d55f33e6a73693b
Ashwin <aswinrajesh1@gmail.com>
2022-03-09 10:01:24 +0530

    Updated from edaplayground

--- tb/testbench.sv
+++ tb/testbench.sv
@@ -1,6 +1,3 @@
-// Code your testbench here
-// or browse Examples
-
 module testbench;
   reg[31:0] r_x = 0;
   reg[31:0] r_y = 0;
@@ -13,15 +10,13 @@ module testbench;
   
   reg[31:0] r_lut_mem[10];
   
-  initial $readmemh("data/arctan_lookup.txt", r_lut_mem);
-  
   reg[4:0] r_shift_amnt = 0;
   
   wire[31:0] w_x;
   wire[31:0] w_y;
   wire[31:0] w_z;
   wire w_d;
-
+  
   reg r_enable = 0;
   
   event e_sync;
@@ -43,20 +38,31 @@ module testbench;
   int i;
   
   initial begin
-    $dumpfile("out/cordic.vcd");
     $dumpvars(0);
+    $dumpfile("dump.vcd");
+	
+    r_lut_mem = {
+      32'h20000000,
+      32'h12E4051D,
+      32'h09FB385B,
+      32'h051111D4,
+      32'h028B0D43,
+      32'h0145D7E1,
+      32'h00A2F61E,
+      32'h00517C55,
+      32'h0028BE53,
+      32'h00145F2E
+    };
     
-    /*
-    Python code to generate lookup table
-    for i in range(0, 10):
-		x = int(np.arctan(2**(-i))*(2**31)/np.pi)
-  		print('32'h0x{0:0{1}X},'.format(x,8))
-    */
-
+	
     @(e_sync);
-    r_x = 32'h0FFFFFFF;
+    r_x = 32'h09B71758;
     r_y = 0;
-    r_z = 32'h20000000;
+    r_z = 32'h15555555;
+
+    #1;
+    $display("0x%8h = %f", r_z, hex_to_degrees(-r_z));
+
     
     r_mode = 1;			// Circular mode
     r_enable = 1;		// Start computation
@@ -64,12 +70,20 @@ module testbench;
     
     @e_sync;
     @e_sync;
+    @e_sync;
+    @e_sync;
+    @e_sync;
+    @e_sync;
+    @e_sync;
+    @e_sync;
+    @e_sync;
+    @e_sync;
     
     r_enable = 0;
     
  	#10 $finish;
   end
-
+  
   always @e_sync if(r_enable) begin
   	r_x <= w_x;
     r_y <= w_y;
@@ -81,4 +95,22 @@ module testbench;
   assign r_lut = r_lut_mem[r_shift_amnt];
   
   always #1 ->e_sync;
+  
+  function real hex_to_degrees(bit[31:0] val);
+    return hex_to_frac(val) * 180;
+  endfunction
+  
+  function real hex_to_frac(bit[31:0] val);
+    int temp;
+    temp = val;
+    return real'(temp) * (2.0 ** -31);
+  endfunction
+  
+  function bit[31:0] frac_to_hex(real frac);
+    return int'(frac * (2.0 ** 31));
+  endfunction
+  
+  function bit[31:0] degrees_to_hex(real degrees);
+    return frac_to_hex(degrees / 180);
+  endfunction
 endmodule
