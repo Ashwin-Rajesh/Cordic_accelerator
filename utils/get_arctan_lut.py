038b14a7828e09f6629731995f3c63b7f1c96ead
Ashwin <aswinrajesh1@gmail.com>
2022-03-09 21:34:18 +0530

    Cleaning up the code

--- utils/get_arctan_lut.py
+++ utils/get_arctan_lut.py
@@ -3,11 +3,13 @@
 import numpy as np
 from conversions import *
 
-fac = 1
+num_iter    = 20        # Number of iterations
 
-for i in range(0, 10):
-    angle = np.arctan(2**(-i))
-    fac   = fac * np.cos(angle)
-    print("32'h{0},".format(hex_from_fraction(fraction_from_rad(angle))))
+fac         = 1         # To maintain multiplication factor to compensate
 
-print("Multiplication factor : 32'h{0}".format(hex_from_fraction(fac)))
+for i in range(0, num_iter):
+    angle = np.arctan(2**(-i))      # Get rotation angle for that iteration
+    fac   = fac * np.cos(angle)     # Get compensation factor for that iteration
+    print("32'h{0},".format(hex_from_fraction(fraction_from_rad(angle))))   # Output the hex format of the rotation angle for that iteration
+
+print("Multiplication factor : 32'h{0} ({1})".format(hex_from_fraction(fac), (fac)))    # Output the compensation multplication factor at the end
