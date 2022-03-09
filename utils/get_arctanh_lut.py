b0dbe7864c90df41baa502ac32e4e4b8a9590ec7
Ashwin <aswinrajesh1@gmail.com>
2022-03-09 22:17:17 +0530

    Fixed 0th iteration

--- utils/get_arctanh_lut.py
+++ utils/get_arctanh_lut.py
@@ -8,8 +8,11 @@ num_iter    = 20        # Number of iterations
 fac         = 1         # To maintain multiplication factor to compensate
 
 for i in range(0, num_iter):
-    angle = np.arctanh(2**(-i))      # Get rotation angle for that iteration
-    fac   = fac * np.cosh(angle)     # Get compensation factor for that iteration
-    print("32'h{0},".format(hex_from_fraction(fraction_from_rad(angle))))   # Output the hex format of the rotation angle for that iteration
+    if(i == 0):
+        print("32'h00000000")               # cant rotate for 0th iteration because arctan(1) = inf
+    else:
+        angle = np.arctanh(2**(-i))         # Get rotation angle for that iteration
+        fac   = fac * np.cosh(angle)        # Get compensation factor for that iteration
+        print("32'h{0},".format(hex_from_fraction(fraction_from_rad(angle))))   # Output the hex format of the rotation angle for that iteration
 
 print("Multiplication factor : 32'h{0} ({1})".format(hex_from_fraction(fac), (fac)))    # Output the compensation multplication factor at the end
