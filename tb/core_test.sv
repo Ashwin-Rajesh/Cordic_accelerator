41562ebd477cc11381037197d87c35b5450c3967
Ashwin <aswinrajesh1@gmail.com>
2022-03-26 12:45:23 +0530

    Added vectoring mode test and cleaned up code

--- tb/core_test.sv
+++ tb/core_test.sv
@@ -3,30 +3,23 @@
 
 `include "types.svh"
 `include "cordic_if.svh"
-
-// Display functions for CORDIC state (x, y, z)
-/*
-function string disp_state_frac_hex(bit[31:0] x, bit[31:0] y, bit[31:0] z);
-  $display("x : %s | y : %s | z : %s", frachex_to_str(x), frachex_to_str(y), anglehex_to_str(z));
-endfunction
-
-function string disp_state_frac(bit[31:0] x, bit[31:0] y, bit[31:0] z);
-  $display("x : %f | y : %f | z : %8f", hex_to_frac(x), hex_to_frac(y), hex_to_degrees(z));
-endfunction
-
-function string disp_state_frac_2(bit[31:0] x, bit[31:0] y, bit[31:0] z);
-  $display("x : %f | y : %f | z : %8f", hex2_to_frac(x), hex2_to_frac(y), hex_to_degrees(z));
-endfunction
-*/
+`include "core_monitor.svh"
+`include "core_driver.svh"
 
 typedef number #(32, 0) fixedpt_1;
 typedef number #(32, 3) fixedpt_2;
 typedef angle #(32) 	ang_type;
 
-function string disp_state(number x, number y, ang_type z);
-  $display("%s, %s, %s", x.to_string(), y.to_string(), z.to_string());
+// Display functions for CORDIC state (x, y, z)
+function string disp_state_frac(bit[31:0] x, bit[31:0] y, bit[31:0] z);
+  $display("%f, %f, %f", fixedpt_1::bin_to_real(x), fixedpt_1::bin_to_real(y), ang_type::bin_to_deg(z));
 endfunction
 
+function string disp_state_frac_2(bit[31:0] x, bit[31:0] y, bit[31:0] z);
+  $display("%f, %f, %f", fixedpt_2::bin_to_real(x), fixedpt_2::bin_to_real(y), ang_type::bin_to_deg(z));
+endfunction
+
+// Display functions using number
 function string disp_state_1(fixedpt_1 x, fixedpt_1 y, ang_type z);
   $display("%f, %f, %f", x.val, y.val, z.val_deg);
 endfunction
@@ -44,47 +37,112 @@ module testbench;
 
   // CORDIC control inputs
   reg r_d 		= 1;  
-  reg r_mode 	= 0;
+  reg r_mode 	= 1;
   reg[4:0] r_shift_amnt = 0;
-    
-  // CORDIC Lookup table
-  reg[31:0] r_lut_mem[20];
-
-  cordic_if #(32) intf();
-
-  assign intf.xprev       = r_x;
-  assign intf.yprev       = r_y;
-  assign intf.zprev       = r_z;
   
-  assign intf.dir         = r_d;
-  assign intf.mode        = r_mode;
-  assign intf.shift_amnt  = r_shift_amnt;
+  // Controller logic variables
+  reg r_mode_control = 1;	// 0 for vectoring, 1 for rotation
+  reg r_enable = 0;			// Enable CORDIC functioning
+
+  // Set lookup table to tan inverse of 2^-i
+  reg[31:0] r_lut_atan[] = {
+    32'h20000000,
+    32'h12E4051D,
+    32'h09FB385B,
+    32'h051111D4,
+    32'h028B0D43,
+    32'h0145D7E1,
+    32'h00A2F61E,
+    32'h00517C55,
+    32'h0028BE53,
+    32'h00145F2E,
+    32'h000A2F98,
+    32'h000517CC,
+    32'h00028BE6,
+    32'h000145F3,
+    32'h0000A2F9,
+    32'h0000517C,
+    32'h000028BE,
+    32'h0000145F,
+    32'h00000A2F,
+    32'h00000517
+  };
+  
+  real p_CIRC_FACTOR = 0.6072529350092496;
+
+  // Set lookup table to tanh inverse of 2^-i
+  reg[31:0] r_lut_atanh[] = {
+    32'h00000000,
+    32'h1661788D,
+    32'h0A680D61,
+    32'h051EA6FC,
+    32'h028CBFDD,
+    32'h01460E34,
+    32'h00A2FCE8,
+    32'h00517D2E,
+    32'h0028BE6E,
+    32'h00145F32,
+    32'h000A2F98,
+    32'h000517CC,
+    32'h00028BE6,
+    32'h000145F3,
+    32'h0000A2F9,
+    32'h0000517C,
+    32'h000028BE,
+    32'h0000145F,
+    32'h00000A2F,
+    32'h00000517
+  };
+  
+  real p_HYP_FACTOR = 1.2051363584457304;
+  
+  // CORDIC-controller interface
+  cordic_if #(32) intf();
+  
+  // Monitors
+  core_monitor #(32, 0) monitor1 = new(intf.controller);	// For circular mode
+  core_monitor #(32, 3) monitor2 = new(intf.controller);	// For hyperbolic mode
 
   // Initializing the CORDIC core
   cordic #(.p_WIDTH(32)) dut (
     intf.core
   );
 
-  // For the control logic to function
-  reg r_enable = 0;
-  
   // Event for triggering a CORDIC iteration
   event e_sync;
 
   // Trigger CORDIC event
   always #2 ->e_sync;
 
-  // Controller logic for rotation
+  // Controller logic for
   always @e_sync if(r_enable) begin
-  	r_x <= intf.xnext;
+    // Data values are shifted from output to input
+    r_x <= intf.xnext;
     r_y <= intf.ynext;
     r_z <= intf.znext;
-    r_d <= ~intf.znext[31];
+    
+	// Increase shift amount by 1
     r_shift_amnt <= r_shift_amnt + 1;
   end
   
+  // Control direction of rotation in next cycle
+  always @(*)
+    if(r_mode_control)
+      r_d <= ~r_z[31];	// Rotation mode
+    else
+      r_d <= r_y[31];	// Vectoring mode
+  
+  // Drive signals
+  assign intf.xprev       = r_x;
+  assign intf.yprev       = r_y;
+  assign intf.zprev       = r_z;  
+  assign intf.dir         = r_d;
+  assign intf.mode        = r_mode;
+  assign intf.shift_amnt  = r_shift_amnt;
+
   // Look up the LUT value for current iteration number (thats equal to the shift amount)
-  assign intf.angle = r_lut_mem[r_shift_amnt];
+  always @(*)
+    intf.angle = r_mode ? r_lut_atan[r_shift_amnt] : r_lut_atanh[r_shift_amnt];
 
   fixedpt_1 num1_x = new(0);
   fixedpt_1 num1_y = new(0);
@@ -92,9 +150,11 @@ module testbench;
   fixedpt_2 num2_x = new(0);
   fixedpt_2 num2_y = new(0);
   
-  ang_type init_angle = new(10);	// Input angle
+  ang_type init_angle = new(0);		// Input angle
 
   ang_type ang_obj = new(0);		// Temporary variable to read from CORDIC outputs
+  
+  real x_exp, y_exp, z_exp;
   
   initial begin
     $dumpvars(0);
@@ -102,140 +162,99 @@ module testbench;
 	
     @e_sync #1;
     
-    r_mode = 1;		// Circular mode
-    // r_mode = 0;		// Hyperbolic mode
+    r_mode = 1;			// Circular mode
+    //r_mode = 0;		// Hyperbolic mode
+    
+    r_mode_control = 1;	// Rotation mode
+    //r_mode_control = 0;	// Vectoring mode
     
     if(r_mode) begin
-      // Circular mode settings
-      
-      // Set lookup table to tan inverse of 2^-i
-      r_lut_mem = {
-        32'h20000000,
-        32'h12E4051D,
-        32'h09FB385B,
-        32'h051111D4,
-        32'h028B0D43,
-        32'h0145D7E1,
-        32'h00A2F61E,
-        32'h00517C55,
-        32'h0028BE53,
-        32'h00145F2E,
-        32'h000A2F98,
-        32'h000517CC,
-        32'h00028BE6,
-        32'h000145F3,
-        32'h0000A2F9,
-        32'h0000517C,
-        32'h000028BE,
-        32'h0000145F,
-        32'h00000A2F,
-        32'h00000517
-      };
-
-      // CORDIC initial data inputs
-      num1_x.set_real(0.6072529350092496);
-      $display("%h", num1_x.val_bin);
-      num1_y.set_real(0);
-      
-      r_x = num1_x.val_bin;
-      r_y = num1_y.val_bin;
-      r_z = init_angle.val_num.val_bin;			// Load angle into z field (in q.31 representation)
-
-      // CORDIC initial control inputs
-      r_d = ~r_z[31];
-      r_shift_amnt = 0;							// Circular rotations must start from i = 0
-
-    end else begin
-      // Hyperbolic mode settings
-      
-      // Set lookup table to tanh inverse of 2^-i
-      r_lut_mem = {
-        32'h00000000,
-        32'h1661788D,
-        32'h0A680D61,
-        32'h051EA6FC,
-        32'h028CBFDD,
-        32'h01460E34,
-        32'h00A2FCE8,
-        32'h00517D2E,
-        32'h0028BE6E,
-        32'h00145F32,
-        32'h000A2F98,
-        32'h000517CC,
-        32'h00028BE6,
-        32'h000145F3,
-        32'h0000A2F9,
-        32'h0000517C,
-        32'h000028BE,
-        32'h0000145F,
-        32'h00000A2F,
-        32'h00000517
-      };
-
-      // CORDIC initial data inputs
-      num2_x.set_real(1.2051363584457304);
-      num2_y.set_real(0);
-      
+      if(r_mode_control) begin
+        $display("Circular rotation");
+        // Circular rotation mode initial data settings
+        num1_x.set_real(p_CIRC_FACTOR);
+        num1_y.set_real(0);
+        init_angle.set_deg(45);
+        
+        x_exp = (num1_x.val * $cos(init_angle.val_rad) - num1_y.val * $sin(init_angle.val_rad)) / p_CIRC_FACTOR;
+        y_exp = (num1_y.val * $cos(init_angle.val_rad) + num1_x.val * $sin(init_angle.val_rad)) / p_CIRC_FACTOR;
+        z_exp = 0;
+      end else begin
+        $display("Circular vectoring");
+        // Circular vectoring mode initial data settings
+        num1_x.set_real(0);
+        num1_y.set_real(0.1);
+        init_angle.set_deg(0);
+        
+        x_exp = (num1_x.val ** 2 + num1_y.val ** 2) ** 0.5 / p_CIRC_FACTOR;
+        y_exp = 0;
+        z_exp = init_angle.val_deg + ($atan2(num1_y.val, num1_x.val) * 180 / $acos(-1));
+      end
+	  r_x = num1_x.val_bin;
+      r_y = num1_y.val_bin;    
+   	end else begin
+      if(r_mode_control) begin
+        $display("Hyperbolic rotation");
+        // Hyperbolic rotation mode initial data settings
+        num2_x.set_real(p_HYP_FACTOR);
+        num2_y.set_real(0);
+        init_angle.set_deg(10);      
+        
+        x_exp = 0;
+        y_exp = 0;
+        z_exp = 0;
+      end else begin
+        $display("Hyperbolic vectoring");
+        // Hyperbolic vectoring mode initial data settings
+        num2_x.set_real(p_HYP_FACTOR);
+        num2_y.set_real(p_HYP_FACTOR);
+        init_angle.set_deg(0);      
+      end
       r_x = num2_x.val_bin;
-      r_y = num2_y.val_bin;
-      r_z = init_angle.val_num.val_bin;
-      
-      // CORDIC initial control inputs
-      r_d = ~r_z[31];
-      r_shift_amnt = 1;							// Hyperbolic rotations must start with 1 (because arctanh(1) = inf)
+      r_y = num2_y.val_bin;    
+   	end
 
-    end
-	
+    r_z = init_angle.val_num.val_bin;			// Load angle into z field (in q.31 representation)
+
+    r_shift_amnt = 0;							// Circular rotations must start from i = 0
+
+		
+    @(e_sync) #1;
+    
     r_enable = 1;								// Start computation
 
     // 20 iterations
     repeat(20) begin
-      if(r_mode) begin
-        num1_x.set_bin(r_x);
-      	num1_y.set_bin(r_y);
-        ang_obj.set_bin(r_z);
-        disp_state_1(num1_x, num1_y, ang_obj);    
-//        disp_state_frac(r_x, r_y, r_z);			// Show x, y in q.31 representation and z in degrees for circular mode
-      end else begin
-		num2_x.set_bin(r_x);
-      	num2_y.set_bin(r_y);
-        ang_obj.set_bin(r_z);
-        disp_state_2(num2_x, num2_y, ang_obj);    
-//        disp_state_frac_2(r_x, r_y, r_z);		// Show x, y in q3.28 representation and z in degrees for hyperbolic mode
-      end
-      @(e_sync) #1;								// Compute 1 iteration of CORDIC
-    end
+      disp_state();								// Display state before each iteration
     
-    if(r_mode) begin
-      num1_x.set_bin(r_x);
-      num1_y.set_bin(r_y);
-      ang_obj.set_bin(r_z);
-      disp_state_1(num1_x, num1_y, ang_obj);    
-//        disp_state_frac(r_x, r_y, r_z);			// Show x, y in q.31 representation and z in degrees for circular mode
-    end else begin
-      num2_x.set_bin(r_x);
-      num2_y.set_bin(r_y);
-      ang_obj.set_bin(r_z);
-      disp_state_2(num2_x, num2_y, ang_obj);    
-//        disp_state_frac_2(r_x, r_y, r_z);		// Show x, y in q3.28 representation and z in degrees for hyperbolic mode
+      @(e_sync) #1;								// Wait for one CORDIC iteration
     end
-
-    if(r_mode) begin
-      $display("cos : %f, sine : %f", $cos(init_angle.val_rad), $sin(init_angle.val_rad));		// Show sine and cosine for circular mode
-      num1_x.set_bin(r_x);
-      num1_y.set_bin(r_y);
-      $display("Error - cos : %e, sine : %e", $cos(init_angle.val_rad) - num1_x.val, $sin(init_angle.val_rad) - num1_y.val);
-    end else begin
-      $display("cosh : %f, sinh : %f", $cosh(init_angle.val_rad), $sinh(init_angle.val_rad));		// Show sinh and cosh for hyperbolic mode
-      num2_x.set_bin(r_x);
-      num2_y.set_bin(r_y);
-      $display("Error - cosh : %e, sinh : %e", $cosh(init_angle.val_rad) - num2_x.val, $sinh(init_angle.val_rad) - num2_y.val);
-    end
-      
+    disp_state();								// Display final state
+    
+    // Compare with expected results
+    $display("Expected results :");
+    $display("%f, %f, %f", x_exp, y_exp, z_exp);
+    
+    $display("Error :");
+    $display("%e, %e, %f deg", fixedpt_1::bin_to_real(r_x) - x_exp, fixedpt_1::bin_to_real(r_y) - y_exp, ang_type::bin_to_deg(r_z) - z_exp);
+    
     r_enable = 0;								// Halt computation
     
  	#10 $finish;								// Finish simulation
   end
+  
+  task disp_state();
+    // $display(r_shift_amnt);
+    if(r_mode) begin
+      monitor1.sample();
+      disp_state_frac(r_x, r_y, r_z);
+      //disp_state_1(monitor1.x_num, monitor1.y_num, monitor1.z_ang);    
+    end else begin
+      monitor2.sample();
+      disp_state_frac_2(r_x, r_y, r_z);
+      //disp_state_2(monitor2.x_num, monitor2.y_num, monitor2.z_ang);    
+    end
+  endtask
   
 endmodule
 
