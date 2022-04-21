8373c44dd08c047d9fcc4f8612d2d33cefcde18e
Harith Manoj <harithpub@gmail.com>
2022-04-21 18:55:41 +0530

    moved design for angle lut outside controller and added requisite ports

--- incl/BusInterface.svh
+++ incl/BusInterface.svh
@@ -3,12 +3,15 @@
 `define BUSINTERFACE_SVH
 
 interface BusInterface #(
-  parameter 	p_WIDTH = 32
+  parameter 	p_WIDTH = 32,
+  localparam 	p_LOG2_WIDTH = $clog2(p_WIDTH)
 );
 
   logic signed [p_WIDTH - 1 : 0] xInput;
   logic signed [p_WIDTH - 1 : 0] yInput;
   logic signed [p_WIDTH - 1 : 0] zInput;
+  logic signed [p_WIDTH - 1 : 0] angle;
+  logic unsigned [p_LOG2_WIDTH - 1 : 0] lutAddress;
   logic unsigned [p_WIDTH - 1 : 0] controlRegisterInput;
 
   logic signed [p_WIDTH - 1 : 0] xResult;
@@ -22,9 +25,14 @@ interface BusInterface #(
   logic interrupt;
 
   modport controller (
-    input xInput, yInput, zInput, controlRegisterInput, clk, rst,
-    output xResult, yResult, zResult, controlRegisterOutput, controlRegisterMask, interrupt
+    input xInput, yInput, zInput, controlRegisterInput, clk, rst, angle,
+    output xResult, yResult, zResult, controlRegisterOutput, controlRegisterMask, interrupt, lutAddress
   );
+
+  modport lut (
+    input lutAddress,
+    output angle
+  )
 
   modport bus (
     output xInput, yInput, zInput, controlRegisterInput, clk, rst,
