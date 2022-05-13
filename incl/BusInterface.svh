6b8fa9d0fb7594516ed17ca979cb4b291e3042b5
Harith Manoj <harithpub@gmail.com>
2022-05-13 20:50:36 +0530

    added control Register bit positions

--- incl/BusInterface.svh
+++ incl/BusInterface.svh
@@ -18,6 +18,31 @@ interface BusInterface #(
   logic unsigned [p_WIDTH - 1 : 0] controlRegisterOutput;
   logic unsigned controlRegisterWriteEnable;
 
+  // ControlRegister : Upper 16 Flags
+  //                 : Lower 16 Control Bits
+
+  // Control  0       : Start
+  //          1       : Stop
+  //          2       : Rotation Mode
+  //          3       : Rotation System 
+  //          4       : Error Interrupt Enable
+  //          5       : Result Interrupt Enable
+  //          6       : Overflow Stop Enable
+  //          7       : Z Overflow stop Enable
+  //          8, 12   : Number of Iteration
+
+  // Flags    16      : Ready
+  //          17      : Error
+  //          18      : Input Error
+  //          19      : Overflow Error
+  //          20      : X Overflow 
+  //          21      : Y Overflow
+  //          22      : Z Overflow
+  //          23, 27  : Iterations Elapsed
+  //          28, 32  : Overflow Iteration
+
+  
+
   logic rst;
   logic clk;
   logic interrupt;
