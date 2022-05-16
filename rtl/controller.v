1e8de46423245f1f36b6e6fc64e2c330198a2c87
Harith Manoj <harithpub@gmail.com>
2022-05-16 15:17:38 +0530

    added cordic, pre cordic stages, modified nextControlRegister aliasing and added localparam for pin numbers in control register

--- rtl/controller.v
+++ rtl/controller.v
@@ -5,8 +5,8 @@
 
     // Control  0       : Start
     //          1       : Stop
-    //          2       : Rotation Mode
-    //          3       : Rotation System 
+    //          2       : p_ROTATION Mode
+    //          3       : p_ROTATION System 
     //          4       : Error Interrupt Enable
     //          5       : Result Interrupt Enable
     //          6       : Overflow Stop Enable
@@ -30,53 +30,67 @@ module Controller #(
 ) (
     BusInterface.controller busPort,
     CordicInterface.controller cordicPort,
-    lutOffset, lutAngle
+    lutOffset, lutAngle, lutSystem
 );
 
-    localparam IDLE = 2'b00;
-    localparam PRE_C = 2'b01;
-    localparam CORDIC = 2'b10;
-    localparam POST_C = 2'b11;
+    localparam p_IDLE = 2'b00;
+    localparam p_PRE_C = 2'b01;
+    localparam p_CORDIC = 2'b10;
+    localparam p_POST_C = 2'b11;
 
-    localparam CIRCULAR = 1'b1;
-    localparam HYPERBOLIC = 1'b0;
+    localparam p_CIRCULAR = 1'b1;
+    localparam p_HYPERBOLIC = 1'b0;
 
-    localparam VECTOR = 1'b0;
-    localparam ROTATION = 1'b1;
+    localparam p_VECTOR = 1'b0;
+    localparam p_ROTATION = 1'b1;
+    localparam p_RIGHT_ANGLE = 32'h40000000;
+    localparam p_LOW_ANGLE = 32'h80000000;
+
+    localparam p_CNTRL_START = 0;
+    localparam p_CNTRL_STOP = 1;
+    localparam p_CNTRL_ROT_MODE = 2;
+    localparam p_CNTRL_ROT_SYS = 3;
+    localparam p_CNTRL_ERR_INT_EN = 4;
+    localparam p_CNTRL_RSLT_INT_EN = 5;
+    localparam p_CNTRL_OV_ST_EN = 6;
+    localparam p_CNTRL_Z_OV_ST_EN = 7;
+    localparam p_CNTRL_ITER_L = 8;
+    localparam p_CNTRL_ITER_H = 12;
+    
+    localparam p_FLAG_READY = 16;
+    localparam p_FLAG_INP_ERR = 17;
+    localparam p_FLAG_OV_ERR = 18;
+    localparam p_FLAG_X_OV_ERR = 19;
+    localparam p_FLAG_Y_OV_ERR = 20;
+    localparam p_FLAG_Z_OV_ERR = 21;
+    localparam p_FLAG_ELAPS_ITER_L = 22;
+    localparam p_FLAG_ELAPS_ITER_H = 26;
+    localparam p_FLAG_OV_ITER_L = 27;
+    localparam p_FLAG_OV_ITER_H = 31;
 
     input [p_WIDTH - 1 : 0 ] lutAngle;
     output wire [p_ANGLE_ADDR_WIDTH - 1:0] lutOffset;
+    output wire lutSystem;
 
-    reg [1:0] controllerState = IDLE;
+    reg [1:0] controllerState = p_IDLE;
     reg [1:0] nextState;
+    reg [31:0] nextControlRegister;
 
-    reg cntrlStart;
-    reg cntrlStop;
-    reg cntrlRotationMode;
-    reg cntrlRotationSystem;
-    reg cntrlErrorIntEn;
-    reg cntrlResultIntEn;
-    reg cntrlOverflowStopEn;
-    reg cntrlZOverflowStopEn;
-    reg [p_ANGLE_ADDR_WIDTH - 1:0] cntrlIterationCount;
-    reg [2:0] cntrlPlaceholder;
-
-    reg flagReady;
-    reg flagInputError;
-    reg flagOverflowError;
-    reg flagXOverflow;
-    reg flagYOverflow;
-    reg flagZOverflow;
-    reg [p_ANGLE_ADDR_WIDTH - 1 : 0] flagIterationElapsed;
-    reg [p_ANGLE_ADDR_WIDTH - 1 : 0] flagOverflowIteration;
-
-    reg [p_WIDTH - 1 : 0] controlRegister = {
+    reg [31 : 0] controlRegister = {
         5'b0, 5'b0, 0, 0, 0, 0, 0, 1, 3'b0, 5'd31, 1, 1, 1, 1, 0, 0, 0, 0
     };
 
-    reg [p_WIDTH - 1 : 0] xResult = 32'h0;
-    reg [p_WIDTH - 1 : 0] yResult = 32'h0;
-    reg [p_WIDTH - 1 : 0] zResult = 32'h0;
+    reg [p_WIDTH - 1 : 0] xValue = 32'h0;
+    reg [p_WIDTH - 1 : 0] yValue = 32'h0;
+    reg [p_WIDTH - 1 : 0] zValue = 32'h0;
+
+    wire [p_WIDTH - 1 : 0] negX, negY, rotZ, absY;
+
+    assign negX = - xValue;
+    assign negY = - yValue;
+    assign absY = (yValue[31])? negY : yValue;
+    assign rotZ[30:0] = zValue[30:0];
+    assign rotZ[31] = ~zValue[31];
     reg contrlWriteEn = 1'b0;
     reg interrupt = 1'b0;
 
@@ -90,61 +104,35 @@ module Controller #(
     wire [p_HALFWORD - 1 : 0] flagUpper;
     wire [p_HALFWORD - 1 : 0] controlLower;
 
-    assign controlLower = { cntrlPlaceholder, cntrlIterationCount, cntrlZOverflowStopEn, 
-    cntrlOverflowStopEn, cntrlResultIntEn, cntrlErrorIntEn, cntrlRotationSystem, 
-    cntrlRotationMode, cntrlStop, cntrlStart};
-
-    assign flagUpper = { flagOverflowIteration, flagIterationElapsed, flagZOverflow, 
-        flagYOverflow, flagXOverflow, flagOverflowError, flagInputError, flagReady};
-    
     assign busPort.controlRegisterOutput = controlRegister;
 
-    
-    assign busPort.xResult = xResult;
-    assign busPort.yResult = yResult;
-    assign busPort.zResult = zResult;
+    assign busPort.xResult = xValue;
+    assign busPort.yResult = yValue;
+    assign busPort.zResult = zValue;
     assign busPort.controlRegisterWriteEnable = contrlWriteEn;
     assign busPort.interrupt = interrupt;
 
-    assign lutOffset = flagIterationElapsed;
+    assign lutOffset = controlRegister[p_FLAG_ELAPS_ITER_H:p_FLAG_ELAPS_ITER_L];
+    assign lutSystem = controlRegister[p_CNTRL_ROT_SYS];
 
-    assign cordicPort.rotationSystem = cntrlRotationSystem;
-    assign cordicPort.rotationAngle = lutAngle;
-    assign cordicPort.shiftAmount = flagIterationElapsed;
+    assign p_CORDICPort.rotationSystem = controlRegister[p_CNTRL_ROT_SYS];
+    assign p_CORDICPort.rotationAngle = lutAngle;
+    assign p_CORDICPort.shiftAmount = controlRegister[p_FLAG_ELAPS_ITER_H:p_FLAG_ELAPS_ITER_L];
 
-    assign cordicPort.rotationDir = rotationDir;
-    assign cordicPort.xPrev = xResult;
-    assign cordicPort.yPrev = yResult;
-    assign cordicPort.zPrev = zResult;
+    assign p_CORDICPort.rotationDir = rotationDir;
+    assign p_CORDICPort.xPrev = xValue;
+    assign p_CORDICPort.yPrev = yValue;
+    assign p_CORDICPort.zPrev = zValue;
 
     wire clk;
     assign clk = busPort.clk;
 
     always @ (*) begin
 
-        nextX = xResult;
-        nextY = yResult;
-        nextZ = zResult;
-
-        cntrlStart = controlRegister[0];
-        cntrlStop = controlRegister[1];
-        cntrlRotationMode = controlRegister[2];
-        cntrlRotationSystem = controlRegister[3];
-        cntrlErrorIntEn = controlRegister[4];
-        cntrlResultIntEn = controlRegister[5];
-        cntrlOverflowStopEn = controlRegister[6];
-        cntrlZOverflowStopEn = controlRegister[7];
-        cntrlIterationCount = controlRegister[12:8];
-
-        flagReady = controlRegister[16];
-        flagInputError = controlRegister[17];
-        flagOverflowError = controlRegister[18];
-        flagXOverflow = controlRegister[19];
-        flagYOverflow = controlRegister[20];
-        flagZOverflow = controlRegister[21];
-        flagIterationElapsed = controlRegister[26:22];
-        flagOverflowIteration = controlRegister[31:27];
-                        
+        nextX = xValue;
+        nextY = yValue;
+        nextZ = zValue;
+        nextControlRegister = controlRegister;          
         nextCntrlWrEn = contrlWriteEn;
         nextInt = interrupt;
 
@@ -153,24 +141,10 @@ module Controller #(
         nextState = controllerState;
 
         if (busPort.rst) begin
-            cntrlStart = 0;
-            cntrlStop = 0;
-            cntrlRotationMode = 0;
-            cntrlRotationSystem = 0;
-            cntrlErrorIntEn = 1;
-            cntrlResultIntEn = 1;
-            cntrlOverflowStopEn = 1;
-            cntrlZOverflowStopEn = 1;
-            cntrlIterationCount = 5'd31;
 
-            flagReady = 1;
-            flagInputError = 0;
-            flagOverflowError = 0;
-            flagXOverflow = 0;
-            flagYOverflow = 0;
-            flagZOverflow = 0;
-            flagIterationElapsed = 5'b0;
-            flagOverflowIteration = 5'b0;
+            nextControlRegister = {
+                5'b0, 5'b0, 0, 0, 0, 0, 0, 1, 3'b0, 5'd31, 1, 1, 1, 1, 0, 0, 0, 0
+            };
 
             nextX = 32'h0;
             nextY = 32'h0;
@@ -180,61 +154,123 @@ module Controller #(
 
             nextRotDir = 0;
 
-            nextState = IDLE;
+            nextState = p_IDLE;
         end else begin
             
             case (controllerState)
-                IDLE : begin
+                p_IDLE : begin
                     nextCntrlWrEn = 1'b0;
                     nextInt = 1'b0;
                     
 
-                    if (busPort.controlRegisterInput[0]) begin
+                    if (busPort.controlRegisterInput[p_CNTRL_START]) begin
                         
                         nextX = busPort.xInput;
                         nextY = busPort.yInput;
                         nextZ = busPort.zInput;
 
-                        cntrlStart = 0;
-
-                        
-                        cntrlRotationMode = busPort.controlRegisterInput[2];
-                        cntrlRotationSystem = busPort.controlRegisterInput[3];
-                        cntrlErrorIntEn = busPort.controlRegisterInput[4];
-                        cntrlResultIntEn = busPort.controlRegisterInput[5];
-                        cntrlOverflowStopEn = busPort.controlRegisterInput[6];
-                        cntrlZOverflowStopEn = busPort.controlRegisterInput[7];
-                        cntrlIterationCount = busPort.controlRegisterInput[12:8];
-
-                        flagReady = 0;
-                        flagInputError = 0;
-                        flagOverflowError = 0;
-                        flagXOverflow = 0;
-                        flagYOverflow = 0;
-                        flagZOverflow = 0;
-                        flagIterationElapsed = 5'b0;
-                        flagOverflowIteration = 5'b0;
+                        nextControlRegister = {
+                            5'b0, 5'b0, 0, 0, 0, 0, 0, 0, 3'b0, 
+                            busPort.controlRegisterInput[p_CNTRL_ITER_H : p_CNTRL_ITER_L], 
+                            busPort.controlRegisterInput[p_CNTRL_Z_OV_ST_EN], 
+                            busPort.controlRegisterInput[p_CNTRL_OV_ST_EN],
+                            busPort.controlRegisterInput[p_CNTRL_RSLT_INT_EN], 
+                            busPort.controlRegisterInput[p_CNTRL_ERR_INT_EN], 
+                            busPort.controlRegisterInput[p_CNTRL_ROT_SYS], 
+                            busPort.controlRegisterInput[p_CNTRL_ROT_MODE], 
+                            0, 0
+                        };
                         
                         nextCntrlWrEn = 1;
 
                         nextRotDir = 0;
 
-                        nextState = IDLE;
+                        nextState = p_PRE_C;
                                                 
                     end
                 end
 
-                PRE_C: begin
+                p_PRE_C: begin
                     nextCntrlWrEn = 0;
                     
+                    nextRotDir = controlRegister[p_CNTRL_ROT_MODE];
+                    nextState = p_CORDIC;
+                    
+                    if(controlRegister[p_CNTRL_ROT_SYS]) begin
+                        nextControlRegister[p_FLAG_ELAPS_ITER_H: p_FLAG_ELAPS_ITER_L] = 5'b00000;
+                    end else begin
+                        nextControlRegister[p_FLAG_ELAPS_ITER_H: p_FLAG_ELAPS_ITER_L] = 5'b00001;
+                    end
+
+                    if (controlRegister[p_CNTRL_ROT_SYS]) begin
+                        if (controlRegister[p_CNTRL_ROT_MODE]) begin
+                            if (zValue > p_RIGHT_ANGLE) begin
+                                nextZ = rotZ;
+                                nextX = negX;
+                                nextY = negY;
+                            end
+                        end else begin
+                            if(xValue[31]) begin
+                                nextZ = rotZ;
+                                nextX = negX;
+                                nextY = negY;
+                            end
+                        end
+                    end
+
+                    if (~controlRegister[p_CNTRL_ROT_SYS]) begin
+                        if(xValue[31] or (absY > xValue)) begin
+                            nextControlRegister[p_FLAG_INP_ERR] = 1;
+                            nextState = p_POST_C;
+                        end
+                    end
+
                 end
+
+                p_CORDIC: begin
+
+                    nextControlRegister[p_FLAG_ELAPS_ITER_H:p_FLAG_ELAPS_ITER_L] 
+                            = controlRegister[p_FLAG_ELAPS_ITER_H:p_FLAG_ELAPS_ITER_L] + 1;
+
+                    if(~controlRegister[p_FLAG_OV_ERR]) begin
+                        nextControlRegister[p_FLAG_OV_ITER_H:p_FLAG_OV_ITER_L]
+                                = controlRegister[p_FLAG_OV_ITER_H:p_FLAG_OV_ITER_L] + 1;
+                    end
+                    
+                    nextX = cordicPort.xResult;
+                    nextY = cordicPort.yResult;
+                    nextZ = cordicPort.zResult;
+                    nextControlRegister[p_FLAG_X_OV_ERR] = cordicPort.xOverflow;
+                    nextControlRegister[p_FLAG_Y_OV_ERR] = cordicPort.yOverflow;
+                    nextControlRegister[p_FLAG_Z_OV_ERR] = cordicPort.zOverflow;
+                    nextControlRegister[p_FLAG_OV_ERR] 
+                        = nextControlRegister[p_FLAG_X_OV_ERR] or nextControlRegister[p_FLAG_Y_OV_ERR]
+                            or nextControlRegister[p_FLAG_Z_OV_ERR];
+
+                    nextState = p_CORDIC;
+
+                    if(busPort.controlRegisterInput[p_CNTRL_STOP]) begin
+                        nextState = p_POST_C;
+                    end
+
+                    if(controlRegister[p_CNTRL_OV_ST_EN]) begin
+                        if(controlRegister[p_FLAG_X_OV_ERR] or controlRegister[p_FLAG_Y_OV_ERR]) begin
+                            nextState = p_POST_C;
+                        end
+                        if(controlRegister[p_CNTRL_Z_OV_ST_EN] and controlRegister[p_FLAG_Z_OV_ERR]) begin
+                            nextState = p_POST_C;
+                        end
+                    end
+
+                    if(controlRegister[p_CNTRL_ITER_H:p_CNTRL_ITER_L] 
+                        == nextControlRegister[p_FLAG_ELAPS_ITER_H:p_FLAG_ELAPS_ITER_L]) begin
+                        nextState = p_POST_C;
+                    end
+                end
+
+                
                 default: 
-            endcase
-            
+            endcase 
         end
-
-        
-
     end
-
 endmodule
